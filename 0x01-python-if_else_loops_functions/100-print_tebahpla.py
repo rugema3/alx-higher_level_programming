ef9f56ee167c5e277e9c63c80091ea8b4ac3dfa9
rugema3 <rugema61@gmail.com>
2023-06-07 02:58:24 +0200

    debugging

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('A') - 1, -1):
-    print(f"{chr(i)}", end='')
+index = 0
+for char in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(char - index)), end="")
+    index = 32 if index == 0 else 0
