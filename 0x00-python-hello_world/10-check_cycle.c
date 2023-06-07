099773512693dcb685cec4e433c7ee2b3786a161
rugema3 <rugema61@gmail.com>
2023-06-07 18:37:50 +0200

    adding comments to explain my logic

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,23 +9,32 @@
  */
 int check_cycle(listint_t *list)
 {
+	/* Initialize two pointers: slow and fast */
 	listint_t *slow, *fast;
 
+	/* If the list is empty or has only one node, there can't be a cycle */
 	if (list == NULL || list->next == NULL)
 		return (0);
 
+	/* Initialize slow & fast pointers to 1st and 2nd nodes, respectively */
 	slow = list;
 	fast = list->next;
 
+	/* Iterate until the fast pointer reaches the end of the list */
 	while (fast != NULL && fast->next != NULL)
 	{
+		/* If there is a cycle, the slow and fast pointers will eventually meet */
 		if (slow == fast)
 			return (1);
 
+		/* Move the slow pointer one step at a time */
 		slow = slow->next;
+
+		/* Move the fast pointer two steps at a time */
 		fast = fast->next->next;
 	}
 
+	/* If the loop completes without finding a cycle, return 0 */
 	return (0);
 }
 
