eb8513370c0d04dc0fe912488572559b7ed466c1
rugema3 <rugema61@gmail.com>
2023-06-08 11:39:56 +0200

    arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+i = 0
+arguments = sys.argv[i:]
+num_args = len(sys.argv) - 1
+print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}:")
+while num_args > 0:
+    if num_args == 1:
+        print(F"{i+1}: {arguments[i+1]}")
+        break
+    else:
+        print(F"{i}: {arguments[i]} ")
+
+    i += 1
+    if i > num_args:
+        break
