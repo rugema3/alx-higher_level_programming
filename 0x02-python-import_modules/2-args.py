028b6ad141abe17683605b34e3b4ef57f0410364
rugema3 <rugema61@gmail.com>
2023-06-08 19:18:26 +0200

    a few modifications in the implementation

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-import sys
-i = 0
-arguments = sys.argv[i:]
-num_args = len(sys.argv) - 1
-print("{} {}:".format(num_args, 'argument' if num_args == 1 else 'arguments'))
-while num_args > 0:
-    if num_args == 1:
-        print("{}: {}".format(i+1, arguments[i+1]))
-        break
-    else:
-        print("{}: {} ".format(i, arguments[i]))
-
-    i += 1
-    if i > num_args:
-        break
+if __name__ == "__main__":
+    import sys
+    i = 0
+    arguments = sys.argv[i:]
+    num = len(sys.argv) - 1
+    print("{} {}:".format(num, 'argument' if num == 1 else 'arguments'))
+    while num > 0:
+        if num == 1:
+            print("{}: {}".format(i+1, arguments[i+1]))
+            break
+        else:
+            print("{}: {} ".format(i, arguments[i]))
+            i += 1
+            if i > num:
+                break
