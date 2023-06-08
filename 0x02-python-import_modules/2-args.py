b268f212aac900cefe6b566106762b02e0e5446e
rugema3 <rugema61@gmail.com>
2023-06-08 19:11:44 +0200

    string formatter issues I think

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,13 @@ import sys
 i = 0
 arguments = sys.argv[i:]
 num_args = len(sys.argv) - 1
-print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}:")
+print("{} {}:".format(num_args, 'argument' if num_args == 1 else 'arguments'))
 while num_args > 0:
     if num_args == 1:
-        print(F"{i+1}: {arguments[i+1]}")
+        print("{}: {}".format(i+1, arguments[i+1]))
         break
     else:
-        print(F"{i}: {arguments[i]} ")
+        print("{}: {} ".format(i, arguments[i]))
 
     i += 1
     if i > num_args:
