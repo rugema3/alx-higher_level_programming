c3a65917e0c1a3cb745ea75f5dd579bd5ec0f51e
rugema3 <rugema61@gmail.com>
2023-06-11 09:57:40 +0200

    using the append method to comply with the requirements

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    for i in range(len(my_list)):
-        for item in my_list:
-            if item % 2 == 0:
-                my_list[i] = True
-            else:
-                my_list[i] = False
+def divisible_by_2(my_list=None):
+    if my_list is None:
+        return None
+
+    result = []
+    for item in my_list:
+        if item % 2 == 0:
+            result.append(True)
+        else:
+            result.append(False)
+
+    return result
