e77fe7f96dec2970d242c1e0d6a0405a9c4d0992
rugema3 <rugema61@gmail.com>
2023-06-11 11:53:22 +0200

    modifications

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -2,25 +2,24 @@
 #include <stdio.h>
 
 /**
- * print_python_list_info - Prints information about a Python list object
+ * print_python_list_info - Prints basic information about a Python list object
  * @p: Pointer to the Python list object
  *
- * Description: This function prints the size and allocation information of
- *              the given Python list object, as well as the type name of the
- *              first five elements in the list.
+ * Description: This function prints the size, allocated memory, and the type of
+ * each element in the Python list.
  */
 void print_python_list_info(PyObject *p)
 {
 	Py_ssize_t size = PyList_Size(p);
 	Py_ssize_t allocated = ((PyListObject *)p)->allocated;
-	PyTypeObject *type = Py_TYPE(p);
 
 	printf("[*] Size of the Python List = %ld\n", size);
 	printf("[*] Allocated = %ld\n", allocated);
-	printf("[*] Element 0: %s\n", Py_TYPE(PyList_GetItem(p, 0))->tp_name);
-	printf("[*] Element 1: %s\n", Py_TYPE(PyList_GetItem(p, 1))->tp_name);
-	printf("[*] Element 2: %s\n", Py_TYPE(PyList_GetItem(p, 2))->tp_name);
-	printf("[*] Element 3: %s\n", Py_TYPE(PyList_GetItem(p, 3))->tp_name);
-	printf("[*] Element 4: %s\n", Py_TYPE(PyList_GetItem(p, 4))->tp_name);
+
+	for (Py_ssize_t i = 0; i < size; i++)
+	{
+		PyObject *item = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+	}
 }
 
