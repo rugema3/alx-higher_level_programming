4830c6518e752d115e983870e861d13a4eac8ce0
rugema3 <rugema61@gmail.com>
2023-06-11 10:07:33 +0200

    tried another aproach to replace items with True or False

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,12 +2,9 @@
 def divisible_by_2(my_list=None):
     if my_list is None:
         return None
-
-    result = []
-    for item in my_list:
-        if item % 2 == 0:
-            result.append(True)
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            my_list[i] = True
         else:
-            result.append(False)
-
-    return result
+            my_list[i] = False
+    return my_list
