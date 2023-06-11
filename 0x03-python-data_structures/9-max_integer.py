30bec28e68a6a52b2073ccff96b5a53856af3cb2
rugema3 <rugema61@gmail.com>
2023-06-11 09:36:15 +0200

    complying with pycodestyle

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if not my_list:
-        return None # return none if the list is empty.
+        return None  # return none if the list is empty.
     else:
         sorted_list = sorted(my_list)
         return sorted_list[-1]
