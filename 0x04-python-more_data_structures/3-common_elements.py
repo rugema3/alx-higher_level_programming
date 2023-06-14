95a29e9d22894ab39cee86637f526d7333504f9d
rugema3 <rugema61@gmail.com>
2023-06-14 11:35:41 +0200

    a function that returns a set of common elements in sets 1 and 2

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    return set_1 & set_2
+
+
+"""
+This function returns the elements present in both sets
+This is commonly known as the intersection of sets.
+"""
