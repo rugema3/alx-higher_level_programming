180cd9fffafa122fd59e660d5090b0f66257c987
rugema3 <rugema61@gmail.com>
2023-06-14 13:44:05 +0200

    updating the dictionary with a new key, value pair if not present or update the exiting key with a new value

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
+
+
+"""
+This function will update the dictionary as follows:
+    If the key is not present in the dictionary
+    It will create a new one. If it exists, it will update it.
+"""
