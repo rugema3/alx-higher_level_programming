465bfcdc856910e2dde72e8c1363492a4832e701
rugema3 <rugema61@gmail.com>
2023-06-14 15:49:33 +0200

    Removed the comments to see if the checker will mark me. Everything runs as expected by the checker not checking everything

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
     return (list(map((lambda x: x * number), my_list)))
-
-
-"""
-This function multplies each element in a list
-by a constant called "number".
-
-It then returns a new list.
-we are using map and lambda to achieve this in exactly 3 lines of code
-"""
