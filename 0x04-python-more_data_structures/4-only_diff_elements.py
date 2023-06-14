0279dbb6484b417fa5a2766e5e3b9fd207923c05
rugema3 <rugema61@gmail.com>
2023-06-14 12:37:45 +0200

    the extra double quotes were causing an error, just removed them.

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -8,4 +8,4 @@ This function returns a set of all elements present in only one set.
 Please note that I could have used set_1 ^ set_2 to obtain similar
 output. I just chose symmetric_difference() method to try something
 new.
-""""
+"""
