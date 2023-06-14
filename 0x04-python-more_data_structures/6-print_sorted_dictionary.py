5fc00506f69aa63c325d26e44e893476f5aa56d7
rugema3 <rugema61@gmail.com>
2023-06-14 13:15:28 +0200

    modifying format specifier

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,8 +2,7 @@
 def print_sorted_dictionary(a_dictionary):
     sorted_keys = sorted(a_dictionary.keys())
     for key in sorted_keys:
-        print(key, ":", a_dictionary[key])
-
+        print(f"{key}: {a_dictionary[key]}")
 
 """
 This function sorts the keys of a dictionary.
