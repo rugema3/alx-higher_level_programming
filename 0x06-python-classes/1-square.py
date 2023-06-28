fa416552193718ef3e0b58a8eb57dc0bae6f05b6
rugema3 <rugema61@gmail.com>
2023-06-28 07:56:38 +0200

    adding more documentation to comply with the requirements

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""Defining a class"""
+
 
 class Square:
+    """Representing a Sqaure"""
+
+
     def __init__(self, size):
+        """Initialise a Sqaure"""
         self.__size = size
