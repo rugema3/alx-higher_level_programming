67a4bd9a2438acfafa53d1cdda7e4653477a5ec0
rugema3 <rugema61@gmail.com>
2023-06-28 12:13:36 +0200

    modification to the print menthod

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -107,10 +107,12 @@ class Square:
         Prints the square using '#' based on the size and position of
         the square.
         """
+
         if self.__size == 0:
-            print()
-        else:
-            for _ in range(self.__position[1]):
-                print()
-            for _ in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+            print("")
+            return
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
