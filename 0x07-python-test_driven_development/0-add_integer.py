499eb657b9301d910ee6ee2d9aa22903b14feabe
rugema3 <rugema61@gmail.com>
2023-07-03 09:49:19 +0200

    pushing an add_integer and the test with differect edge cases. I have used doctest for the tests. The tests are located in a seperate .txt file

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+def add_integer(a, b=98):
+    """Compute and returns the sum of two numbers
+
+    Args:
+        a (integer): A number to be added.
+        b (integer): A number to be added.
+    Returns:
+        integer: a number representing the sum of a and b
+    Raises:
+        TypeError: if either a or b is not an int or float.
+
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")   
+    else:
+        a = int(a)
+        b = int(b)
+        return a + b
+        
