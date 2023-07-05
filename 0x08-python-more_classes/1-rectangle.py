261337dc5ce805e69a6258b0b06aa05ea204812f
rugema3 <rugema61@gmail.com>
2023-07-05 19:25:55 +0200

    updating my getters to make them private. making them self.__height and self.__width

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -29,7 +29,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
@@ -43,4 +43,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
+        self.__height = value
