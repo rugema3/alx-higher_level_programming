42e9f46db9d0b55b62bdd8dab5bf8d137e903b16
rugema3 <rugema61@gmail.com>
2023-07-05 17:14:25 +0200

    complying with pydocstyle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-""" Defining class rectangle"""
+"""Defining class Rectangle."""
 
 
 class Rectangle:
-    """
-    This is an empty class called Rectangle.
-    """
+    """This is an empty class called Rectangle."""
+
     def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle instance."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Get the width of the Rectangle."""
         return self._width
 
     @width.setter
     def width(self, value):
+        """Set the width of the Rectangle."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -24,10 +26,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """Get the height of the Rectangle."""
         return self._height
 
     @height.setter
     def height(self, value):
+        """Set the height of the Rectangle."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
