2bf5d339faf445cf8863a3af07bb9a7bc528f740
rugema3 <rugema61@gmail.com>
2023-07-05 19:32:52 +0200

    updating how the height and width will be called

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,21 +6,14 @@ class Rectangle:
     """This is an empty class called Rectangle."""
 
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle instance.
-
-        Args:
-            width (int): The width of the  rectangle.
-            height (int): The height of the rectangle.
-
-
-        """
+        """Initialize a new Rectangle instance."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
         """Get the width of the Rectangle."""
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -34,7 +27,7 @@ class Rectangle:
     @property
     def height(self):
         """Get the height of the Rectangle."""
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
