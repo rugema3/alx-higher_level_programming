8da00e19c57747d4f80d29a5c4eae8a006c8ac6b
rugema3 <rugema61@gmail.com>
2023-07-05 18:32:43 +0200

    adding a logic that handles when  either width or height of a rectangle is zero to return zero

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -51,4 +51,6 @@ class Rectangle:
 
     def perimeter(self):
         """Calculate and return the perimeter of a rectangle."""
+        if self.width == 0 or self.height == 0:
+            return 0
         return 2 * (self.width + self.height)
