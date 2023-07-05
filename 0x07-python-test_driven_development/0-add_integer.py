0cfbab0a4b22f6debf48815a9b23aceee2cb00f1
rugema3 <rugema61@gmail.com>
2023-07-05 11:22:17 +0200

    adding a test for floatoverflow errors

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 def add_integer(a, b=98):
     """Compute and returns the sum of two numbers
 
@@ -14,6 +16,7 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
     else:
         a = int(a)
         b = int(b)
