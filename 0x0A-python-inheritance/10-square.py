d871b0ac3e34f05c07c7da3a9cfcdcfbb6fdec96
rugema3 <rugema61@gmail.com>
2023-07-13 01:31:38 +0200

    Square #1 this inherits from the Rectangle class. inherittance inside another inheritance

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A class representing a square, inheriting from Rectangle."""
+
+    def __init__(self, size):
+        """
+        Initialize a Square instance.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than or equal to 0.
+        """
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        """
+        Return a string representation of the square.
+
+        Returns:
+            str: The string representation of the square.
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
