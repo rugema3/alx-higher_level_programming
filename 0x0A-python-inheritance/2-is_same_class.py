578310f45cecadb0ce571444f6250427e2c91aea
rugema3 <rugema61@gmail.com>
2023-07-12 20:48:40 +0200

    a function that checks if an object is belongs to a certain class, it returns True if True, False otherwise

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""2-is_same_class module."""
+
+
+def is_same_class(obj, a_class):
+    """Define is_same_class.
+
+    Args:
+    obj: an objec to be checked.
+    a_class: a class to compare if an object belongs to.
+    Return: True if an object belongs to class otherwise False.
+    """
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
