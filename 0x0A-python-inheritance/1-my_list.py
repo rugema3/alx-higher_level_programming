9e35614391a9d3cf366b673933936ba7fb93b985
rugema3 <rugema61@gmail.com>
2023-07-12 20:08:15 +0200

    my algorithm was off. I made a copy of the list and then sorted the list of the copied list. This sorting will not affect the original list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,5 +7,6 @@ class MyList(list):
 
     def print_sorted(self):
         """Print the list in ascending order."""
-        sorted_list = sorted(self)
+        sorted_list = self.copy()
+        sorted_list.sort()
         print(sorted_list)
