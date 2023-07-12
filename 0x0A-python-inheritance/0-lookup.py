d13381bd8c217c9c660a2e421e7dfd2925904990
rugema3 <rugema61@gmail.com>
2023-07-12 16:10:49 +0200

    a function that returns the list of available attributes and methods of an object. I have added its test with different cases

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""0-lookup module."""
+
+
+def lookup(obj):
+    """Define lookup method.
+    
+    Args:
+    obj: object to be parsed as an argument.
+
+    Description:
+    =============================================
+    This function will return a list of available
+    attributes and methods of an object.
+    """
+    if not isinstance(obj, object):
+        raise ValueError("Invalid object. Object must"
+                         "be an instance of a class.")
+
+    try:
+        return dir(obj)
+    except Exception as e:
+        print("Error occurred during attribute/method lookup:", str(e))
+        return []
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,2 @@
+<h1>0x0A. Python - Inheritance</h1>
+
