1d94c530b231968b1a381be473a615fd23832524
rugema3 <rugema61@gmail.com>
2023-07-13 22:44:36 +0200

    a script that adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,35 +1,25 @@
 #!/usr/bin/python3
-"""Script to add arguments to a list and save to a JSON file."""
-
-
+"""7-add_item module."""
 import sys
-from typing import List
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+import json
 
+def save_to_json_file(data, filename):
+    """Save data to a JSON file."""
+    with open(filename, 'w') as file:
+        json.dump(data, file)
 
-def add_arguments_to_list(arguments: List[str]):
-    """
-    Add arguments to a list and save to a JSON file.
-
-    Args:
-        arguments: A list of arguments to add to the existing data list.
-
-    """
-    try:
-        # Load existing data from file
-        existing_data = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        existing_data = []
-
-    # Add new arguments to the list
-    existing_data.extend(arguments)
-
-    # Save the updated list to file
-    save_to_json_file(existing_data, "add_item.json")
-
+def load_from_json_file(filename):
+    """Load data from a JSON file."""
+    with open(filename, 'r') as file:
+        return json.load(file)
 
 if __name__ == "__main__":
-    # Skip the script name argument and pass the rest to add_arguments_to_list
-    arguments = sys.argv[1:]
-    add_arguments_to_list(arguments)
+    filename = "add_item.json"
+
+    try:
+        items = load_from_json_file(filename)
+    except FileNotFoundError:
+        items = []
+
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, filename)
