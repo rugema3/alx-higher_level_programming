b71f749cdf7fbd8b95e622c27fb9157bd0d604e8
rugema3 <rugema61@gmail.com>
2023-07-17 04:31:42 -0700

    addin g the updates

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,7 +9,7 @@ class Base:
 
     def __init__(self, id=None):
         """Init method of the base Class."""
-        if sel.id is not None:
+        if self.id is not None:
             self.id = id
         else:
             __nb_objects = __nb_objects + 1
