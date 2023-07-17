d53bfee98cd8947100d955ff5f68d99d46a5ee8f
rugema3 <rugema61@gmail.com>
2023-07-17 04:26:10 -0700

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Base module."""
+
+
+class Base:
+    """Define class named Base."""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Init method of the base Class."""
+        if sel.id is not None:
+            self.id = id
+        else:
+            __nb_objects = __nb_objects + 1
+            self.id = __nb_objects
