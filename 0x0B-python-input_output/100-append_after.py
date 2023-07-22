b62bfc3989a41d5b98e6c5e63fe68d0fe850dfbf
rugema3 <rugema61@gmail.com>
2023-07-22 12:10:07 +0200

    updating the 100-append_after to meet the requirements.

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -13,12 +13,14 @@ def append_after(filename="", search_string="", new_string=""):
     if not filename or not search_string or not new_string:
         return
 
-    lines_list = []
     with open(filename, 'r') as file:
         lines_list = file.readlines()
 
-    updated_lines = [line + new_string + '\n' if search_string in line
-                     else line for line in lines_list]
+    updated_lines = []
+    for line in lines_list:
+        updated_lines.append(line)
+        if search_string in line:
+            updated_lines.append(new_string)
 
     with open(filename, 'w') as file:
         file.writelines(updated_lines)
