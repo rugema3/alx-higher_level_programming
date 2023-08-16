456680a6c62ad09587a100edca535ec5b7717f59
rugema3 <rugema61@gmail.com>
2023-08-16 10:43:20 -0700

    complying with the assignment requirements, i have added to create a table if it doesnt exist.

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,15 +1,15 @@
 -- A script that creates a table second_table in the database
 
-CREATE TABLE second_table (
-	id INT,
-	name VARCHAR(256),
-	score INT
+CREATE TABLE IF NOT EXISTS `second_table` (
+	`id` INT,
+	`name` VARCHAR(256),
+	`score` INT
 );
 
 -- insert into the table
-INSERT INTO second_table (id, name, score)
+INSERT INTO `second_table` (`id`, `name`, `score`)
 VALUES 
-	(1, 'John', 10),
-	(2, 'Alex', 3),
-	(3, 'Bob', 13),
-	(4, 'George', 8);
+	(1, "John", 10),
+	(2, "Alex", 3),
+	(3, "Bob", 13),
+	(4, "George", 8);
