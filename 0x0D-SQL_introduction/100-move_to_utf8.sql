9f7ed9e7eeb03bae06dc631c1717a0446b7e6b6b
rugema3 <rugema61@gmail.com>
2023-08-16 12:01:14 -0700

    using a different approach

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,14 +1,5 @@
 -- Alter the database
 
 -- Select the database you wanan work with
-USE hbtn_0c_0;
-
--- Convert the given database to UTF8 (utf8mb4)
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the fist_table to UTF8 (utf8mb4)
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the name field in first_table to UTF8 (utf8mb4)
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
+USE `hbtn_0c_0`;
+ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
