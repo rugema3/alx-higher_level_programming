b16f15306764de5b5c4f54bcd30588401f17c55f
rugema3 <rugema61@gmail.com>
2023-08-17 22:06:43 -0700

    create a table if it doesn't exit and assign attributyes to it

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,8 @@
+-- Create a table with certain attributes.
+
+-- Create the table if it doesn't exeist already.
+CREATE TABLE IF NOT EXISTS force_name (
+	id INT,
+	name VARCHAR(256) NOT NULL
+	);
+
