dba1b0f6803267b6c9b026f6713f789c313ff0bf
rugema3 <rugema61@gmail.com>
2023-08-17 23:31:28 -0700

    Create a table and then create a table in it called cities with a primary key as id which auto_increments and the primary key of that table. Then we referenced the state_+id as the foregin key.

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,16 @@
+-- A script that creates the database hbtn_0d_usa and the table cities 
+-- (in the database hbtn_0d_usa) on your MySQL server.
+
+-- Create the database hbtn_0d_usa if it doen't exist.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Select the database to work with.
+USE hbtn_0d_usa;
+
+-- Create a table cities in the selected db.
+CREATE TABLE IF NOT EXISTS cities (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256),
+	state_id INT,
+	FOREIGN KEY (state_id) REFERENCES states(id)
+	);
