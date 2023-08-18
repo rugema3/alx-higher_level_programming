d6bc6a1c6564374321f1f03fee538854d70b16be
rugema3 <rugema61@gmail.com>
2023-08-18 00:20:15 -0700

    Listing cities with their corresponding states

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Listing all cities in the database.
+
+-- List all cities and their corresponding state names
+SELECT cities.id, cities.name, states.name AS state_name
+FROM cities
+INNER JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
