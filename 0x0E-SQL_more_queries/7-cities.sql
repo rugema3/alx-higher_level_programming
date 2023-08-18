951a92af222fa4b0580e28d0e85ca9c470b23894
rugema3 <rugema61@gmail.com>
2023-08-17 23:42:43 -0700

    name must not be null as well

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -10,7 +10,7 @@ USE hbtn_0d_usa;
 -- Create a table cities in the selected db.
 CREATE TABLE IF NOT EXISTS cities (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(256),
+	name VARCHAR(256) NOT NULL,
 	state_id INT NOT NULL,
 	FOREIGN KEY (state_id) REFERENCES states(id)
 	);
