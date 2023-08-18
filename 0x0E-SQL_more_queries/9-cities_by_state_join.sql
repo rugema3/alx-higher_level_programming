28a04e504316962d851366ffb49763ce85cd2ae1
rugema3 <rugema61@gmail.com>
2023-08-18 00:22:57 -0700

    using left join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,5 +3,5 @@
 -- List all cities and their corresponding state names
 SELECT cities.id, cities.name, states.name AS state_name
 FROM cities
-INNER JOIN states ON cities.state_id = states.id
+LEFT JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
