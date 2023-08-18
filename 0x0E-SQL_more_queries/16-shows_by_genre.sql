87442a1869d60c1771f685cce4901653fce4212e
rugema3 <rugema61@gmail.com>
2023-08-18 01:33:21 -0700

    lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- List all shows and their linked genres
+SELECT
+    tv_shows.title,
+    GROUP_CONCAT(tv_genres.name ORDER BY tv_genres.name ASC SEPARATOR ', ') AS genres
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_shows.title
+ORDER BY tv_shows.title ASC, genres ASC;
