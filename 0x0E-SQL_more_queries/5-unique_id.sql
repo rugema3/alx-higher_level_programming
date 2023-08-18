4e940a15b34975d91f7c95d822bebed562b44c27
rugema3 <rugema61@gmail.com>
2023-08-17 22:47:44 -0700

    createing a table witha  unique id that has a default value of 1

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,7 @@
+-- A script that creates the table unique_id on your MySQL server.
+
+-- Create the table called unique_id.
+CREATE TABLE IF NOT EXISTS unique_id (
+	id INT DEFAULT 1 UNIQUE,
+	name VARCHAR(256)
+	);
