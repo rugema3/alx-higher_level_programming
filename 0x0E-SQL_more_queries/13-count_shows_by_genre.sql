5b424f8095732f892431eeac86293a4fc71334aa
rugema3 <rugema61@gmail.com>
2023-08-18 01:16:52 -0700

    using right join approach

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,10 +1,11 @@
--- List all genres along with the number of shows linked to each genre
+-- List all genres along with the number of shows linked to each genres
 
 -- List all genres along with the number of shows linked to each genre
 SELECT
     tv_genres.name AS genre,
     COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
 ORDER BY number_of_shows DESC;
+
