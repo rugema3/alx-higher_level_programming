a6725fbf0b0592a75e023d1b910ef8676ff2e1f5
rugema3 <rugema61@gmail.com>
2023-09-12 00:22:30 +0200

    using modules and accessing them. here I have created trhe add function that adds two integers and then call the function in another file

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/**
+ * adds two numbers.
+ * @param {number} num1 - The first number
+ * @param {number} num2 - The second number
+ * @returns {number} - the sum of num1 and num2
+ */
+function add (num1, num2) {
+  return num1 + num2;
+}
+module.exports.add = add;
