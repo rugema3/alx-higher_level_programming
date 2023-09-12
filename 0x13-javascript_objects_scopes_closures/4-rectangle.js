dc9086fbe126350d0ba1769d4a185c6ddaa2647e
rugema3 <rugema61@gmail.com>
2023-09-12 20:32:08 +0200

    Created an instance method called rotate() that exchanges the width and the height of the rectangle Created an instance method called double() that multiples the width and the height of the rectangle by 2

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,69 @@
+#!/usr/bin/node
+/*
+  This class represents a Rectangle.
+
+  It has a constructor that takes two arguments:
+  - 'w' for width
+  - 'h' for height
+
+  The constructor initializes the instance attributes 'width' and 'height'
+  with the values of 'w' and 'h', respectively.
+*/
+class Rectangle {
+  constructor (w, h) {
+    /*
+      Inside the constructor:
+      - 'this' refers to the newly created instance of the Rectangle class.
+      - 'this.width' is an instance attribute that represents the width of the rectangle.
+      - 'this.height' is an instance attribute that represents the height of the rectangle.
+
+      The constructor initializes the 'width' and 'height' attributes with the values
+      passed as arguments 'w' and 'h' when creating a new Rectangle instance.
+    */
+
+    /*
+      Check if 'w' and 'h' are positive integers.
+      If either is not a positive integer or if either is 0, create an empty object.
+      Otherwise, initialize the 'width' and 'height' attributes.
+    */
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  /*
+   * This method prints the rectangle using 'X' characters.
+   * It loops through the height and width of the rectangle to create the pattern.
+  */
+  print () {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+
+  /*
+   * This method exchanges the width and height of the rectangle.
+  */
+  rotate () {
+    if (this.width && this.height) {
+      const temp = this.width;
+      this.width = this.height;
+      this.height = temp;
+    }
+  }
+
+  /*
+   * This method multiplies the width and height of the rectangle by 2.
+  */
+  double () {
+    if (this.width && this.height) {
+      this.width *= 2;
+      this.height *= 2;
+    }
+  }
+}
+
+module.exports = Rectangle;
