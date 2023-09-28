7b635146267a05f22efa36643a9fef7a9e46a225
rugema3 <rugema61@gmail.com>
2023-09-28 18:18:53 +0200

    modifications

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/bash
-#  a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-url="$1"
-curl -sL "$url"
+# A Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -sL "$1"
