08f3e5eb4fb5959892916ce6d56f201ce1a862c2
rugema3 <rugema61@gmail.com>
2023-09-28 12:05:17 +0200

    a shell script that  takes in a URL, sends a request to that URL, and displays the size of the body of the response

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# A bash script that takes in a URL, sends a request to that URL, 
+# and displays the size of the body of the response
+
+# Check if the URL argument is provided
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+# Get the URL from the command-line argument
+URL="$1"
+
+# Use curl to fetch the URL and pipe the output to wc to count bytes
+response_size=$(curl -s "$URL" | wc -c)
+
+# Display the size of the response body in bytes
+echo "Size of the response body: $response_size bytes"
