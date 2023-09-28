fd6896ecb0c55713a33e33cc26b4a56d868bea36
rugema3 <rugema61@gmail.com>
2023-09-28 19:59:53 +0200

    modification to my code.

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -s -X POST -H "Content-Type: application/json" --data @"$2" "$1"
+curl -s POST -H "Content-Type: application/json" --data @"$2" "$1"
