b372cb6d682bf117efb97e006129200390a81d3a
rugema3 <rugema61@gmail.com>
2023-09-28 19:10:02 +0200

    The checker doesn't seem to like the approach used earlier. I am parsing the parameters directly.

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-# Define the variables to be sent in the POST request
-EMAIL="test@gmail.com"
-SUBJECT="I will always be here for PLD"
-
-# Use curl to send a POST request with the specified variables and display the response body
-curl -s -X POST -d "email=$EMAIL&subject=$SUBJECT" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
