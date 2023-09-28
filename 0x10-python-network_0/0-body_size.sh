74208cd7ecff1bdc78c771b57cdbe9f8b526ff49
rugema3 <rugema61@gmail.com>
2023-09-28 12:14:58 +0200

    modifications

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A bash script that takes in a URL, sends a request to that URL, 
-# and displays the size of the body of the response
+# Get the byte size of the HTTP response header 
 curl -s "$1" | wc -c
