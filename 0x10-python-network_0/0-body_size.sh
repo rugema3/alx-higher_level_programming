45a641becc2d21d217a795514d9ca176a20d2a7e
rugema3 <rugema61@gmail.com>
2023-09-28 12:13:20 +0200

    modifications

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # A bash script that takes in a URL, sends a request to that URL, 
 # and displays the size of the body of the response
-
-# Get the URL from the command-line argument
-URL="$1"
-
-# Use curl to fetch the URL and pipe the output to wc to count bytes
-curl -s "$URL" | wc -c
+curl -s "$1" | wc -c
