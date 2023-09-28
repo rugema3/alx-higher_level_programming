cae5fd73dc3cd27c87e13a0126aafc197db10b0b
rugema3 <rugema61@gmail.com>
2023-09-28 12:11:56 +0200

    modify

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,12 +2,6 @@
 # A bash script that takes in a URL, sends a request to that URL, 
 # and displays the size of the body of the response
 
-# Check if the URL argument is provided
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
 # Get the URL from the command-line argument
 URL="$1"
 
