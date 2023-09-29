639fa2b976a2049dacec390e28540fb6b40d74f6
rugema3 <rugema61@gmail.com>
2023-09-29 20:40:58 +0200

    going back to my original approach

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,25 +2,44 @@
 
 """
 A script that:
-- displays the X-Request-Id header variable of a request to a given URL
+- takes in a URL
+- sends a request to the URL
+- displays the value of the X-Request-Id variable in the response header
 """
 
 import sys
 import requests
 
 
-def get_x_request_id(url):
+def fetch_x_request_id(url):
     """
-    Fetches the specified URL and returns the value of the
-    X-Request-Id header.
-
+    Fetches a URL and displays the value of the X-Request-Id
+    variable in the response header.
     Args:
         url (str): The URL to fetch.
-
+    Returns:
+        None
     """
-    r = requests.get(url)
-    print(r.headers.get("X-Request-Id"))
+    try:
+        # Send a GET request to the specified URL
+        response = requests.get(url)
 
-if __name__ == '__main__':
-    url = url = sys.argv[1]
-    get_e_request_id(url)
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Get the value of the X-Request-Id header
+            x_request_id = response.headers.get('X-Request-Id')
+
+            if x_request_id:
+                print(x_request_id)
+            else:
+                print("X-Request-Id header not found in the response.")
+        else:
+            print("Error: HTTP Status Code", response.status_code)
+    except requests.exceptions.RequestException as e:
+        # Handle any request-related errors
+        print("Error:", e)
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    fetch_x_request_id(url)
